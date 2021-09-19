4a8792834ec27f7587c69aa0a7bde6188d6527cf
Adrian Heng <adrianhyk@gmail.com>
2021-09-19 20:02:14 +0800

    add boilerplate

---
+++ bin/geddit.dart
@@ -0,0 +1,6 @@
+import 'package:geddit/geddit.dart';
+
+void main() {
+
+  print('This is the main binary');
+}
---
+++ lib/geddit.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library geddit;
+
+export 'src/geddit_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
